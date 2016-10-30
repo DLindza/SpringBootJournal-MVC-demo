53a746bf822c324d027511138d6784dbb90daf48
johnde02 <devon@MacBook-Pro.local>
2016-10-30 18:11:16 -0400

    security working

--- src/main/java/lindsay/devon/spring/config/InMemorySecurityConfiguration.java
+++ src/main/java/lindsay/devon/spring/config/InMemorySecurityConfiguration.java
@@ -3,5 +3,20 @@ package lindsay.devon.spring.config;
 /**
  * Created by devon on 10/30/16.
  */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+
+@Configuration
+@EnableGlobalAuthentication
 public class InMemorySecurityConfiguration {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER")
+                .and().withUser("admin").password("password").roles("USER", "ADMIN");
+    }
+
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,11 @@
 # This can be useful for demos and for testing if you are careful,
 # but probably not something you want to be on the classpath in production.
 
+--USERS IN JOURNAL
+INSERT INTO ACCOUNT(account_name, password) VALUES('springboot', 'isawesome');
+INSERT INTO ACCOUNT(account_name, password) VALUES('springsecurity', 'isawesometoo');
+
+--JOURNAL DATA
 INSERT INTO ENTRY (title,summary,created) VALUES ('Get to know Spring Boot', 'Today I will learn Spring Boot', ' 2016-10-30 00:00:00.00');
 INSERT INTO ENTRY (title,summary,created) VALUES ('Simple Spring Boot Project', 'I will do my first Spring Boot project', ' 2016-10-30 00:00:00.00');
 INSERT INTO ENTRY (title,summary,created) VALUES ('Spring Boot Reading', 'Read more About Spring Boot', ' 2016-10-30 00:00:00.00');
