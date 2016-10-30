b594058cbcdedaf7f76ccb94e7697007213c7d10
johnde02 <devon@MacBook-Pro.local>
2016-10-30 18:41:14 -0400

    secure resource login

---
+++ src/main/java/lindsay/devon/spring/config/ResourceSecurityConfiguration.java
@@ -0,0 +1,28 @@
+package lindsay.devon.spring.config;
+
+/**
+ * Created by devon on 10/30/16.
+ */
+
+//THIS PROVIDES SECURITY FOR YOUR RESOURCES / DATABASE QUEIRIES / HELPS PREVENT RANDOM PEOPLE
+    //ACCESSING THE POST , PUT AND DELETE FUNCTIONALITY
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalAuthentication
+public class ResourceSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .and()
+                //.httpBasic();
+                .formLogin();
+    }
+}
